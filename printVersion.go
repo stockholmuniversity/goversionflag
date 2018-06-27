c7231e778b35822f88822a323ae54bbce9ecab7b
Björn Bohman <bjorn.bohman@su.se>
2018-06-27 09:01:59 +0200

    Remove code that checks arguments and operands after flag.Parse()

--- printVersion.go
+++ printVersion.go
@@ -58,6 +58,7 @@ func PrintVersionAndExit() {
 	// Check remaining arguments if we got 'version'. This is needed because
 	// " Flag parsing stops just before the first non-flag argument "
 	// This way --version can come anywhere in the argument list and still honour the GNU coding standard.
+    /*
 	remainingArguments := flag.Args()
 	if len(remainingArguments) > 0 {
 		for _, argument := range remainingArguments {
@@ -66,6 +67,7 @@ func PrintVersionAndExit() {
 			}
 		}
 	}
+    */
 
 	if *arg == true || fakeFlag == true {
 		buildversion := GetBuildInformation()
